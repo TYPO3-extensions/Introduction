2bcdfcd598578da652d40d9c7dbb6c8fef09f55e
mastermind <mastermind@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-22 12:53:35 +0000

    Updated the createDump script with the one used by Ernesto when creating dump for 4.5b2 release. Resolves #11578
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introduction/trunk@41400 735d13b6-9817-0410-8766-e36946ffe9aa

--- scripts/createDump.sh
+++ scripts/createDump.sh
@@ -1,43 +1,90 @@
 #!/bin/sh
-TABLES="be_groups be_users_shadow fe_groups fe_users index_fulltext index_grlist index_phash index_rel index_section index_stat_word index_words pages pages_language_overlay sys_be_shortcuts sys_filemounts sys_language sys_news sys_refindex sys_template tt_content tt_news tt_news_cat tt_news_cat_mm tx_realurl_pathcache tx_realurl_uniqalias tx_realurl_urldecodecache tx_realurl_urlencodecache"
 
 # Tables which have a field deleted. Used to clean up deleted records before exporting
-CLEANUP_TABLES="be_groups be_users_shadow fe_groups fe_users pages pages_language_overlay sys_filemounts sys_news sys_refindex sys_template tt_content tt_news tt_news_cat"
-DATABASE=typo3
+CLEANUP_TABLES=`xargs <<EOF
+be_groups
+be_users_shadow
+fe_groups
+fe_users
+pages
+pages_language_overlay
+sys_filemounts
+sys_news
+sys_refindex
+sys_template
+tt_content tt_news
+tt_news_cat
+EOF`
+
+TRUNCATE_TABLES=`xargs <<EOF
+be_sessions
+cache_extensions
+cache_hash
+cache_imagesizes
+cache_md5params
+cache_pages
+cache_pagesection
+cache_treelist
+cache_typo3temp_log
+cachingframework_cache_hash
+cachingframework_cache_hash_tags
+cachingframework_cache_pages
+cachingframework_cache_pages_tags
+cachingframework_cache_pagesection
+cachingframework_cache_pagesection_tags
+fe_session_data
+fe_sessions
+index_stat_search
+index_stat_word
+sys_history
+sys_lockedrecords
+sys_log
+tt_news_cache
+tt_news_cache_tags
+tx_extbase_cache_object
+tx_extbase_cache_object_tags
+tx_extbase_cache_reflection
+tx_extbase_cache_reflection_tags
+tx_linkvalidator_links
+tx_realurl_chashcache
+tx_realurl_errorlog
+tx_realurl_pathcache
+tx_realurl_urldecodecache
+tx_realurl_urlencodecache
+EOF`
+
+DATABASE=introduction45
 OUTPUTFILE=introduction.sql
-USER=root
-PASSWORD=1234
 
 # Only dump the backend users we need
-mysql -u ${USER} --password=${PASSWORD} ${DATABASE} -e 'DROP TABLE IF EXISTS be_users_shadow; CREATE TABLE be_users_shadow SELECT * FROM be_users WHERE uid IN(2,3,4); ALTER TABLE be_users_shadow ADD PRIMARY KEY (uid), ADD KEY parent (pid), ADD KEY username (username);ALTER TABLE be_users_shadow CHANGE uid uid INT(11) unsigned NOT NULL auto_increment;'
+mysql ${DATABASE} -e '
+DROP TABLE IF EXISTS be_users_shadow;
+CREATE TABLE be_users_shadow SELECT * FROM be_users WHERE uid IN(2,3,4);
+ALTER TABLE be_users_shadow ADD PRIMARY KEY (uid), ADD KEY parent (pid), ADD KEY username (username);
+ALTER TABLE be_users_shadow CHANGE uid uid INT(11) unsigned NOT NULL auto_increment;
+'
 
 # Remove deleted records
 for table in ${CLEANUP_TABLES}
 do
-	mysql -u ${USER} --password=${PASSWORD} ${DATABASE} -e "DELETE FROM ${table} WHERE deleted=1;"
+	mysql ${DATABASE} -e "DELETE FROM ${table} WHERE deleted=1;"
 done
 
-# Dump the tables we need
-mysqldump -u ${USER} -p${PASSWORD} --disable-keys --skip-quote-names ${DATABASE} ${TABLES} | sed 's/AUTO_INCREMENT=[0-9]* //' > ${OUTPUTFILE}_dump
+# Truncate cache tables
+for table in ${TRUNCATE_TABLES}
+do
+	mysql ${DATABASE} -e "TRUNCATE ${table};"
+done
+
+# Dump all tables and do some cleanup
+mysqldump --disable-keys --skip-quote-names ${DATABASE} | sed \
+	-e 's/AUTO_INCREMENT=[0-9]* //' \
+	-e 's/be_users_shadow/be_users/g' \
+	-e "s/ CHARACTER SET utf8//g" \
+	-e "s/ DEFAULT CHARSET=latin1/ DEFAULT CHARSET=utf8/g" \
+	-e "s/absRefPrefix/\# absRefPrefix/g" \
+	-e "s/tx_realurl_enable = 1/tx_realurl_enable = \#\#\#ENABLE_REALURL\#\#\#/g" \
+	-e "s/domain = [^\\]*/domain = ###HOSTNAME_AND_PATH###/g" > ${OUTPUTFILE}
 
 # Cleanup temporary table
-mysql -u ${USER} --password=${PASSWORD} ${DATABASE} -e 'DROP TABLE IF EXISTS be_users_shadow;'
-
-# Rename table be_users_shadow to be_users
-sed "s/be_users_shadow/be_users/g" ${OUTPUTFILE}_dump > ${OUTPUTFILE}_shadow
-
-# Remove character set from table copy
-sed "s/ CHARACTER SET utf8//g" ${OUTPUTFILE}_shadow> ${OUTPUTFILE}_character
-sed "s/ DEFAULT CHARSET=latin1/ DEFAULT CHARSET=utf8/g" ${OUTPUTFILE}_character> ${OUTPUTFILE}_character2
-
-# Comment absRefPrefix
-sed "s/absRefPrefix/\# absRefPrefix/g" ${OUTPUTFILE}_character2 > ${OUTPUTFILE}_absRefPrefix
-
-# Replace ENABLE REALURL
-sed "s/tx_realurl_enable = 1/tx_realurl_enable = \#\#\#ENABLE_REALURL\#\#\#/g" ${OUTPUTFILE}_absRefPrefix > ${OUTPUTFILE}_realURL
-
-# Replace Site path
-sed "s/domain = [^\\]*/domain = ###HOSTNAME_AND_PATH###/g" ${OUTPUTFILE}_realURL > ${OUTPUTFILE}
-
-# Cleanup
-rm ${OUTPUTFILE}_absRefPrefix ${OUTPUTFILE}_character ${OUTPUTFILE}_character2 ${OUTPUTFILE}_dump ${OUTPUTFILE}_realURL ${OUTPUTFILE}_shadow
+mysql ${DATABASE} -e 'DROP TABLE IF EXISTS be_users_shadow;'
